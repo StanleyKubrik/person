bec8cb57e1d7f71732e95f4254f03986db2d77bf
StanleyKubrik <sukhonosov.nikita@gmail.com>
2018-12-20 21:01:59 +0200

    Correct Import

--- src/constanta/Constanta.java
+++ src/constanta/Constanta.java
@@ -2,5 +2,6 @@ package constanta;
 
 public abstract class Constanta {
     public static final String URL = "jdbc:h2:~/test";
-    public static final String PATH = "C:\\Users\\Nikita\\IdeaProjects\\Person\\person.txt";
+    public static final String PATH_IN = "D:\\Nikita\\Person\\person.txt";
+    public static final String PATH_OUT = "person.txt";
 }
--- src/model/filereader/FileReader.java
+++ src/model/filereader/FileReader.java
@@ -11,31 +11,24 @@ public class FileReader implements FileReaderContract {
     private FileInputStream fileInputStream;
     private FileOutputStream fileOutputStream;
 
-    public FileReader() throws FileNotFoundException {
-        fileInputStream = new FileInputStream(Constanta.PATH);
-        fileOutputStream = new FileOutputStream(Constanta.PATH);
-    }
-
     @Override
     public void writer(String text) throws IOException {
-        if (fileOutputStream != null) {
-            byte[] buffer = text.getBytes();
-            fileOutputStream.write(buffer,0,buffer.length);
-            fileOutputStream.close();
-        }
+        fileOutputStream = new FileOutputStream(Constanta.PATH_OUT);
+        byte[] buffer = text.getBytes();
+        fileOutputStream.write(buffer,0,buffer.length);
+        fileOutputStream.close();
     }
 
     @Override
     public String reader() throws IOException {
+        fileInputStream = new FileInputStream(Constanta.PATH_IN);
         StringBuilder text = new StringBuilder();
-        if(fileInputStream != null){
-            byte[] buffer = new byte[1024];
-            while(fileInputStream.read() != -1) {
-                String bug = new String(buffer, StandardCharsets.UTF_8);
-                text.append(bug);
-            }
-            fileInputStream.close();
+        byte[] buffer = new byte[1024];
+        while(fileInputStream.read() != -1) {
+            String bug = new String(buffer, StandardCharsets.UTF_8);
+            text.append(bug);
         }
+        fileInputStream.close();
         return text.toString();
     }
 }
