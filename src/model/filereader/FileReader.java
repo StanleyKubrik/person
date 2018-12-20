b6bc695ea953dace7633246af60ef8600c2d8d3f
ДуырсрÑLechshenko Yuri <lechenko.yura@gmail.com>
2018-12-20 22:58:13 +0200

    edits

--- src/model/filereader/FileReader.java
+++ src/model/filereader/FileReader.java
@@ -1,41 +1,44 @@
 package model.filereader;
 
 import constanta.Constanta;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 public class FileReader implements FileReaderContract {
     private FileInputStream fileInputStream;
     private FileOutputStream fileOutputStream;
 
     public FileReader() throws FileNotFoundException {
-        fileInputStream = new FileInputStream(Constanta.PATH);
-        fileOutputStream = new FileOutputStream(Constanta.PATH);
     }
 
     @Override
     public void writer(String text) throws IOException {
+        fileOutputStream = new FileOutputStream(Constanta.PATH);
         if (fileOutputStream != null) {
             byte[] buffer = text.getBytes();
-            fileOutputStream.write(buffer,0,buffer.length);
+            fileOutputStream.write(buffer, 0, buffer.length);
             fileOutputStream.close();
         }
     }
 
     @Override
     public String reader() throws IOException {
+        fileInputStream = new FileInputStream(Constanta.PATH);
         StringBuilder text = new StringBuilder();
-        if(fileInputStream != null){
-            byte[] buffer = new byte[1024];
-            while(fileInputStream.read() != -1) {
-                String bug = new String(buffer, StandardCharsets.UTF_8);
-                text.append(bug);
-            }
-            fileInputStream.close();
+        byte[] buffer = new byte[1024];
+        //TODO было так  while(fileInputStream.read() != -1)
+        //TODO вот какой у меня пример на репе что я скидывал 35 строка while((inputStream.read(buffer)) != -1)
+        while ((fileInputStream.read(buffer)) != -1) {
+            String bug = new String(buffer, UTF_8);
+            text.append(bug);
         }
+        fileInputStream.close();
         return text.toString();
     }
 }
